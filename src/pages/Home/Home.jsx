73608d3b6d30b74cd41bc2974ac81e2714c18559
sonyokpara <sony.okpara@yahoo.com>
2025-06-06 07:01:07 +0000

    Create Hero section mockup

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,23 @@
 import Navbar from '../../components/Navbar/Navbar'
 import './Home.css'
+import heroBanner from '../../assets/hero_banner.jpg'
+import heroTitle from '../../assets/hero_title.png'
+
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
+      <div className="hero">
+        <img src={heroBanner} alt="" className="banner-img" />
+        <div className="hero-caption">
+          <img src={heroTitle} alt="" className="caption-img" />
+          <p>
+            Discovering his ties to a secret ancient order, a young man living
+            in modern Istanbul embarks on a quest to save the city from an
+            immortal enemy.
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
